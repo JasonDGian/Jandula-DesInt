d1257231d34381be394bd5261fabcf81c0b6b183
JasonDGian <dgia543@g.educaand.es>
2024-10-02 08:09:07 +0200

    ejericio

--- Pruebas/test3/lib/main.dart
+++ Pruebas/test3/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test3/config/theme/app_theme.dart';
+import 'package:test3/presentation/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,17 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: AppTheme(selectedColor: 5).theme(), // New de tipo inferido.
       title: 'Yes No App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Yes No App'),
-        ),
-        body:  Center(
-          child: FilledButton.tonal(
-            onPressed: (){}, 
-            child: const Text('Click me')
-            ),
-        ),
-      ),
+      home: const ChatScreen(),
     );
   }
 }
